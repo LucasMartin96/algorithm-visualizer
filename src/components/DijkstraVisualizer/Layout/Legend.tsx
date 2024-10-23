8546ecb3f853ffd449dd928f8338a3a972dc47c0
lmartin <lucasmartin766@gmail.com>
2024-10-23 17:40:44 -0300

    again to build!

--- src/components/DijkstraVisualizer/Layout/Legend.tsx
+++ src/components/DijkstraVisualizer/Layout/Legend.tsx
@@ -1,6 +1,6 @@
 interface LegendItem {
-  color: string
-  description: string
+  color: string;
+  description: string;
 }
 
 const legendItems: LegendItem[] = [
@@ -8,20 +8,22 @@ const legendItems: LegendItem[] = [
   { color: 'bg-blue-500', description: 'Start Node' },
   { color: 'bg-red-500', description: 'End Node' },
   { color: 'bg-green-500', description: 'Visited Node' },
-  { color: 'bg-yellow-500', description: 'Path Node' }
-]
+  { color: 'bg-yellow-500', description: 'Path Node' },
+];
 
 const Legend = () => {
   return (
     <div className="flex flex-col gap-2 p-4 bg-gray-100 rounded-lg">
       {legendItems.map((item, index) => (
         <div key={index} className="flex items-center gap-2">
-          <div className={`w-6 h-6 ${item.color} border border-gray-300 rounded`} />
+          <div
+            className={`w-6 h-6 ${item.color} border border-gray-300 rounded`}
+          />
           <span>{item.description}</span>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Legend
+export default Legend;
